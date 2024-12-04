9c5d26618760d792d4f9364efe3b57a938a22eaf
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2024-12-04 08:29:03 +0000

    Task one complete

---
+++ main.js
@@ -0,0 +1,60 @@
+// 1. Favorite Foods
+const favoriteFoods = ["Cheese", "Chocolate", "Olives", "Anchovies"];
+
+// 2. Favorite Songs
+const favoriteSongs = [
+  "Tiny Dancer",
+  "Running Up That Hill",
+  "Don't Stop Moving",
+  "Tragedy",
+  "Crazy Horses"
+];
+
+const pennyDoublingProfit = [ 1, 2 , 4 , 8 , 16 , 32 , 64 ];
+
+
+
+const friends = [
+ {name:Chris , specialSkill: "changing lives"},
+ {name:Lizzie , specialSkill: "being friendly"},
+ {name:Ben , specialSkill: "code rapping"},
+ {name:Tao , specialSkill: "hand raising"},
+ {name:Liz , specialSkill: "crochet"},
+ {name:Patrick , specialSkill: "Call of Duty"},
+ {name: Tim , specialSkill: "analogies"},
+ {name: James , specialSkill: "friendship"},
+ {name: Joseph , specialSkill: "being positive"},
+ {name: Max , specialSkill: "papier-mâché"}
+
+]
+
+
+
+
+
+/* ## Task 2 - Reading Array Values
+
+```js
+let options = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+```
+
+Using bracket notation and the correct index, for each person...
+
+👉 Console.log the month in which you were born.
+
+👉 Console.log the month your mother was born.
+
+👉 Console.log your favorite month. */
